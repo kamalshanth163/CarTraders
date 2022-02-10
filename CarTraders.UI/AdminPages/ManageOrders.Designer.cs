19bb8b8f54397aa029036ca8b3c2ecaf2e6cc270
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-02-10 11:02:15 +0530

    Add manage orders form

---
+++ CarTraders.UI/AdminPages/ManageOrders.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CarTraders.UI.AdminPages
+{
+    partial class ManageOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.menuBtn = new System.Windows.Forms.Button();
+            this.carsDataView = new System.Windows.Forms.DataGridView();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Red;
+            this.exitBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.Location = new System.Drawing.Point(692, 28);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(39, 31);
+            this.exitBtn.TabIndex = 35;
+            this.exitBtn.Text = "X";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // menuBtn
+            // 
+            this.menuBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.menuBtn.Location = new System.Drawing.Point(581, 30);
+            this.menuBtn.Name = "menuBtn";
+            this.menuBtn.Size = new System.Drawing.Size(105, 29);
+            this.menuBtn.TabIndex = 34;
+            this.menuBtn.Text = "Go to menu";
+            this.menuBtn.UseVisualStyleBackColor = true;
+            this.menuBtn.Click += new System.EventHandler(this.menuBtn_Click);
+            // 
+            // carsDataView
+            // 
+            this.carsDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.carsDataView.Location = new System.Drawing.Point(306, 82);
+            this.carsDataView.Name = "carsDataView";
+            this.carsDataView.RowTemplate.Height = 60;
+            this.carsDataView.Size = new System.Drawing.Size(461, 343);
+            this.carsDataView.TabIndex = 33;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.deleteBtn.Location = new System.Drawing.Point(155, 396);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(68, 29);
+            this.deleteBtn.TabIndex = 32;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.updateBtn.Location = new System.Drawing.Point(69, 396);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(76, 29);
+            this.updateBtn.TabIndex = 31;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.title.Location = new System.Drawing.Point(247, 25);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(172, 37);
+            this.title.TabIndex = 30;
+            this.title.Text = "Manage Cars";
+            // 
+            // ManageOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.menuBtn);
+            this.Controls.Add(this.carsDataView);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManageOrders";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ManageOrders";
+            ((System.ComponentModel.ISupportInitialize)(this.carsDataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button menuBtn;
+        private System.Windows.Forms.DataGridView carsDataView;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Label title;
+    }
+}
---
+++ CarTraders.UI/AdminPages/ManageOrders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarTraders.UI.AdminPages
+{
+    public partial class ManageOrders : Form
+    {
+        public ManageOrders()
+        {
+            InitializeComponent();
+        }
+
+        private void menuBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
