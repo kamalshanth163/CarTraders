7fe018c313081fedc3a258fd1135c4e774e7f4d1
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-02-12 11:51:09 +0530

    UI designed for CartPage

--- CarTraders.UI/CustomerPages/CartPage.Designer.cs
+++ CarTraders.UI/CustomerPages/CartPage.Designer.cs
@@ -29,9 +29,8 @@ namespace CarTraders.UI.CustomerPages
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CartPage));
             this.title = new System.Windows.Forms.Label();
-            this.exitBtn = new System.Windows.Forms.Button();
-            this.storeBtn = new System.Windows.Forms.Button();
             this.cart_list = new System.Windows.Forms.FlowLayoutPanel();
             this.total_price = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,47 +40,27 @@ namespace CarTraders.UI.CustomerPages
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.storeBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // title
             // 
             this.title.AutoSize = true;
             this.title.Font = new System.Drawing.Font("Segoe UI", 20F);
-            this.title.Location = new System.Drawing.Point(33, 14);
+            this.title.Location = new System.Drawing.Point(123, 34);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(126, 37);
             this.title.TabIndex = 38;
             this.title.Text = "Your Cart";
             // 
-            // exitBtn
-            // 
-            this.exitBtn.BackColor = System.Drawing.Color.Red;
-            this.exitBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.exitBtn.ForeColor = System.Drawing.Color.White;
-            this.exitBtn.Location = new System.Drawing.Point(741, 17);
-            this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(39, 32);
-            this.exitBtn.TabIndex = 52;
-            this.exitBtn.Text = "X";
-            this.exitBtn.UseVisualStyleBackColor = false;
-            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
-            // 
-            // storeBtn
-            // 
-            this.storeBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.storeBtn.Location = new System.Drawing.Point(630, 19);
-            this.storeBtn.Name = "storeBtn";
-            this.storeBtn.Size = new System.Drawing.Size(105, 30);
-            this.storeBtn.TabIndex = 51;
-            this.storeBtn.Text = "Go to store";
-            this.storeBtn.UseVisualStyleBackColor = true;
-            this.storeBtn.Click += new System.EventHandler(this.storeBtn_Click);
-            // 
             // cart_list
             // 
             this.cart_list.AutoScroll = true;
             this.cart_list.BackColor = System.Drawing.Color.Transparent;
-            this.cart_list.Location = new System.Drawing.Point(48, 103);
+            this.cart_list.Location = new System.Drawing.Point(119, 150);
             this.cart_list.Name = "cart_list";
             this.cart_list.Size = new System.Drawing.Size(700, 272);
             this.cart_list.TabIndex = 53;
@@ -89,10 +68,10 @@ namespace CarTraders.UI.CustomerPages
             // total_price
             // 
             this.total_price.AutoSize = true;
-            this.total_price.Font = new System.Drawing.Font("Segoe UI", 20F);
-            this.total_price.Location = new System.Drawing.Point(220, 388);
+            this.total_price.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.total_price.Location = new System.Drawing.Point(293, 452);
             this.total_price.Name = "total_price";
-            this.total_price.Size = new System.Drawing.Size(32, 37);
+            this.total_price.Size = new System.Drawing.Size(33, 37);
             this.total_price.TabIndex = 54;
             this.total_price.Text = "0";
             // 
@@ -100,7 +79,7 @@ namespace CarTraders.UI.CustomerPages
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label2.Location = new System.Drawing.Point(43, 395);
+            this.label2.Location = new System.Drawing.Point(116, 459);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(172, 25);
             this.label2.TabIndex = 55;
@@ -108,20 +87,23 @@ namespace CarTraders.UI.CustomerPages
             // 
             // checkoutBtn
             // 
-            this.checkoutBtn.Font = new System.Drawing.Font("Segoe UI", 16F);
-            this.checkoutBtn.Location = new System.Drawing.Point(554, 386);
+            this.checkoutBtn.BackColor = System.Drawing.Color.Gold;
+            this.checkoutBtn.FlatAppearance.BorderSize = 0;
+            this.checkoutBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkoutBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.checkoutBtn.Location = new System.Drawing.Point(627, 450);
             this.checkoutBtn.Name = "checkoutBtn";
             this.checkoutBtn.Size = new System.Drawing.Size(194, 41);
             this.checkoutBtn.TabIndex = 56;
             this.checkoutBtn.Text = "Order";
-            this.checkoutBtn.UseVisualStyleBackColor = true;
+            this.checkoutBtn.UseVisualStyleBackColor = false;
             this.checkoutBtn.Click += new System.EventHandler(this.checkoutBtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label1.Location = new System.Drawing.Point(69, 79);
+            this.label1.Location = new System.Drawing.Point(140, 126);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 21);
             this.label1.TabIndex = 57;
@@ -131,7 +113,7 @@ namespace CarTraders.UI.CustomerPages
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label3.Location = new System.Drawing.Point(436, 79);
+            this.label3.Location = new System.Drawing.Point(507, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 21);
             this.label3.TabIndex = 58;
@@ -141,7 +123,7 @@ namespace CarTraders.UI.CustomerPages
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label4.Location = new System.Drawing.Point(350, 79);
+            this.label4.Location = new System.Drawing.Point(421, 126);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 21);
             this.label4.TabIndex = 59;
@@ -151,7 +133,7 @@ namespace CarTraders.UI.CustomerPages
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label5.Location = new System.Drawing.Point(140, 79);
+            this.label5.Location = new System.Drawing.Point(211, 126);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 21);
             this.label5.TabIndex = 60;
@@ -161,17 +143,59 @@ namespace CarTraders.UI.CustomerPages
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label6.Location = new System.Drawing.Point(533, 79);
+            this.label6.Location = new System.Drawing.Point(604, 126);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(85, 21);
             this.label6.TabIndex = 61;
             this.label6.Text = "Price (LKR)";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(49, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 69;
+            this.pictureBox1.TabStop = false;
+            // 
+            // storeBtn
+            // 
+            this.storeBtn.BackColor = System.Drawing.Color.White;
+            this.storeBtn.FlatAppearance.BorderSize = 0;
+            this.storeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.storeBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.storeBtn.Location = new System.Drawing.Point(765, 31);
+            this.storeBtn.Name = "storeBtn";
+            this.storeBtn.Size = new System.Drawing.Size(105, 29);
+            this.storeBtn.TabIndex = 68;
+            this.storeBtn.Text = "Go to store";
+            this.storeBtn.UseVisualStyleBackColor = false;
+            this.storeBtn.Click += new System.EventHandler(this.storeBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.Image = ((System.Drawing.Image)(resources.GetObject("exitBtn.Image")));
+            this.exitBtn.Location = new System.Drawing.Point(870, 29);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(39, 31);
+            this.exitBtn.TabIndex = 67;
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
             // CartPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(945, 550);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.storeBtn);
+            this.Controls.Add(this.exitBtn);
             this.Controls.Add(this.checkoutBtn);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -181,14 +205,13 @@ namespace CarTraders.UI.CustomerPages
             this.Controls.Add(this.label2);
             this.Controls.Add(this.total_price);
             this.Controls.Add(this.cart_list);
-            this.Controls.Add(this.exitBtn);
-            this.Controls.Add(this.storeBtn);
             this.Controls.Add(this.title);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CartPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CartPage";
             this.Load += new System.EventHandler(this.CartPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,8 +220,6 @@ namespace CarTraders.UI.CustomerPages
         #endregion
 
         private System.Windows.Forms.Label title;
-        private System.Windows.Forms.Button exitBtn;
-        private System.Windows.Forms.Button storeBtn;
         private System.Windows.Forms.FlowLayoutPanel cart_list;
         private System.Windows.Forms.Label total_price;
         private System.Windows.Forms.Label label2;
@@ -208,5 +229,8 @@ namespace CarTraders.UI.CustomerPages
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button storeBtn;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
